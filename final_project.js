b3269c117a67d2095627d8af729771204f789927
CaitlinED <102256735+CaitlinED@users.noreply.github.com>
2022-05-19 16:45:15 -0400

    Add files via upload

--- final_project.js
+++ final_project.js
@@ -15,12 +15,25 @@ function letsGo(e) {
 greeting.innerText = `Welcome, ${name.value}`
 hello.style.display = 'block';
 form.style.display = "none"
-body.style.backgroundImage = 'url("https://images.pexels.com/photos/1038001/pexels-photo-1038001.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load")';
 greeting.classList.add('fade')
-setTimeout(displayNav, 6000)
+setTimeout(displayNav, 4000)
 }
+
 function displayNav() {
     const nav = document.querySelector('nav')
     nav.style.display = 'block'
+    setTimeout(displayTopcontainer, 100)
+    setTimeout(removeHello, 50)
+}
 
+function displayTopcontainer() {
+    const topcontainer = document.querySelector('.topcontainer')
+    topcontainer.style.display = 'block'
+   
+}
+
+function removeHello(){
+    const hello = document.querySelector('h1')
+    console.log('hello')
+    hello.style.display = 'none'
 }
